dbaa910b2c188b3862367fe12d1f8452f7fc6237
Augusto Carvalho <augustogany@gmail.com>
2020-08-05 22:33:13 +0000

    refactory landy-page

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -10,21 +10,7 @@ class FrontEndController extends Controller
 {
     function default()
     {
-
-        $page = setting('site.page');
-        $collection = Page::where('slug', $page)->first();
-        
-        if($collection){
-            $blocks = Block::where('page_id', $collection->id)->orderBy('position', 'asc')->get();
-            return view($collection->direction, [
-                'collection' => json_decode($collection->details, true),
-                'page' => $collection,
-                'blocks'     => $blocks
-            ]);
-        }else{
-            return view('welcome');
-        }
-        
+      return view('layouts.frontend.welcome');        
     }
     public function pages($slug)
     {
